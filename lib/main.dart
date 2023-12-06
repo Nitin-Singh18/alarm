7ac778e4592762bb38781e8a8c824a1dc44725c5
karan <i.nitinsingh18@gmail.com>
2023-12-06 17:09:24 +0530

    set up

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:alarm_app/view/home/home_view.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Alarm App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomeView(),
+    );
+  }
+}
